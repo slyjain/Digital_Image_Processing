7b488858517fc6e83c8da13351436f3a04bf467d
slyjain <jain.samyak.3210@gmail.com>
2024-08-02 16:28:21 +0000

    Add tambola.py code to DIP

---
+++ tambola.py
@@ -0,0 +1,38 @@
+
+import random
+import numpy as np
+tambola = np.zeros((3, 9))
+
+def createTambolaRow(n):
+    count = 0
+    hashArray = [0] * n
+    while count < 5:
+        random_int = random.randint(0, 8)  
+        if hashArray[random_int] == 0:
+            hashArray[random_int] = 1
+            count += 1
+    return hashArray
+for i in range(3):
+  tambola[i]=createTambolaRow(9);
+  
+def hashArrayGenerator():
+    all_hash_arrays = []
+    for i in range(9):
+        hashArray = [0] * 10
+        count = 0
+        while count < 3:
+            random_int = random.randint(0, 9)  
+            if hashArray[random_int] == 0:
+                hashArray[random_int] = 1
+                count += 1
+        all_hash_arrays.append(hashArray)
+    return all_hash_arrays
+hashArray=hashArrayGenerator();
+for i in range(9):
+  k=0
+  for j in range(10):
+      if hashArray[i][j]==1:
+        tambola[k][i]=(i*10+j+1)*tambola[k][i];
+        k+=1;
+print(tambola)
+
